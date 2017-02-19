0765bb8225b98cb98a5121ee274342681f0c00cd
chsunil <ch.sunilchowdary@gmail.com>
2017-02-19 09:25:51 +0530

    commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -261,7 +261,7 @@ export class DashboardComponent implements OnInit {
       label: 'Comments'
     }
   ];
-  public mainChartLabels:Array<any> = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'];
+  public mainChartLabels:Array<any> = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28'];
   public mainChartOptions:any = {
     responsive: true,
     maintainAspectRatio: false,
