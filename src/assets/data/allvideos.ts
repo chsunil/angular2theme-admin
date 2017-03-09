dcf06f49d386fd20ffc69a16e9d742aa5329d3bb
sunilch <sunil.challagundla@vedicsys.com>
2017-03-09 15:25:25 +0530

    update major

--- src/assets/data/allvideos.ts
+++
@@ -1,6 +0,0 @@
-export var films = [
-    { vtitle: 'Project - This is example of project	', user: 'Patrick Smith', date:'Jul 14, 2013',shares:'shares',favorites:'favorites',comments:'comments',  actions:'Hold Delete'},
-     { vtitle: 'Project - This is example of project	', user: 'Patrick Smith', date:'Jul 14, 2013',shares:'shares',favorites:'favorites',comments:'comments', actions:'Hold Delete'},
-      { vtitle: 'Project - This is example of project	', user: 'Patrick Smith', date:'Jul 14, 2013',shares:'shares',favorites:'favorites',comments:'comments',  actions:'Active Delete'},
-
-];
